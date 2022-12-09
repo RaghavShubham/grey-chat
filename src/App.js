3eff141528f6a9cde9a8bf6d2142d0292c868293
Shubham Raghav <shubham3raghav@gmail.com>
2022-12-09 14:37:58 +0530

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import "./App.css";
+import Login from "./screens/auth/login";
+import Register from "./screens/auth/register";
+import Home from "./screens/home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./shared/context";
 
 function App() {
-  return <div className="App">Grey Chat</div>;
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) return <Navigate to="/login" />;
+    else return children;
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
