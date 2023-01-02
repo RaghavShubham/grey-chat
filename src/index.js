42875ad1167fee1e3ed4724a44250c41be55d656
Shubham Raghav <shubham3raghav@gmail.com>
2023-01-03 04:35:48 +0530

    adds Chat Context

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { AuthContextProvider } from "./shared/context";
+import { AuthContextProvider, ChatContextProvider } from "./shared/context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChatContextProvider>
   </AuthContextProvider>
 );
--- src/shared/context.js
+++ src/shared/context.js
@@ -1,8 +1,15 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { createContext, useState, useEffect } from "react";
+import {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  useReducer,
+} from "react";
 import { auth } from "../firebase";
 
 export const AuthContext = createContext();
+export const ChatContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
@@ -20,5 +27,36 @@ export const AuthContextProvider = ({ children }) => {
     <AuthContext.Provider value={{ currentUser }}>
       {children}
     </AuthContext.Provider>
+  );
+};
+
+export const ChatContextProvider = ({ children }) => {
+  const INITIAL_STATE = { chatId: "null", user: {} };
+
+  const { currentUser } = useContext(AuthContext);
+
+  const chatReducer = (state, action) => {
+    console.log(action.payload);
+    switch (action.type) {
+      case "CHANGE_USER":
+        return {
+          user: action.payload,
+          chatId:
+            currentUser.uid > action.payload.uid
+              ? currentUser.uid + action.payload.uid
+              : action.payload.uid + currentUser.uid,
+        };
+
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
+
+  return (
+    <ChatContext.Provider value={{ data: state, dispatch }}>
+      {children}
+    </ChatContext.Provider>
   );
 };
